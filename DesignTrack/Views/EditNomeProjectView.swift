b26fa1e9680a94da7200ff9ef40dca02973378a6
Juac01 <julcamoro1998@hotmail.com>
2025-02-13 17:53:17 -0300

    Editar Nome OK, e Termos Ok

---
+++ DesignTrack/Views/EditNomeProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  EditNomeProjectView.swift
+//  DesignTrack
+//
+//  Created by JOSE JOAQUIN JULCAMORO BUSTAMANTE on 13/02/25.
+//
+
+import SwiftUI
+
+struct EditNomeProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditNomeProjectView()
+}
